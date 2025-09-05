71b8f7e00bce63ef1858296e97e48abccf62bf26
Yurchenko Elena <echistova@gmail.com>
2025-09-05 14:20:14 +0300

    feat: towelsort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  return matrix
+    .map((row, i) => {
+      if (i % 2 === 1) {
+        return row.slice().reverse();
+      }
+      return row;
+    })
+    .flat();
 };
